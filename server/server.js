e3d6f5571ed1069ac180919d22a9ec29b1ff379c
kristen3119 <kristen3119@gmail.com>
2017-08-20 19:59:36 -0400

    GET todos

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+//var {MongoClient} = require("mongodb"); 
+
 var {mongoose} = require("./db/mongoose.js")
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user")
@@ -15,10 +17,22 @@ app.post("/todos", function(req, res){
     })
     todo.save(function(err, docs){
     	if(err){return res.status(400).send(err)}
-    		res.send(docs)
+    	res.send(docs)
     })
+})
+
+app.get('/todos', function(req, res){
+	Todo.find(function(err, todos){
+if(err){res.status(400).send(err)}
+	res.send({todos})
+
+})
 })
 
 app.listen(3000, function(){
 	console.log("app running on port 3000")
 })
+
+
+
+module.exports = {app};
